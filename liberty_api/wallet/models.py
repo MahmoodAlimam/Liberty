668ec2a1d9ba252d0aac7ab9a17df39444df736b
MahmoodAlimam <mahmood.alimam@gmail.com>
2022-09-03 10:19:10 +0100

    Adding UPreference table and modify few fields in User Table

--- liberty_api/wallet/models.py
+++ liberty_api/wallet/models.py
@@ -14,16 +14,24 @@ class User(models.Model):
     )
     gender = models.CharField(max_length=1, choices=SEX)
     mobile_number = PhoneNumberField(null=False, blank=False, unique=True)
-    email = models.CharField(max_length=40, default='zzzz@yyy')
+    email = models.EmailField(max_length=40, default='zzzz@yyy')
 
     dob = models.DateField(null=False,default=django.utils.timezone.now)
     nationality = models.CharField(max_length=20, default='Japan')
 
-    security_question1 = models.CharField(null=True, max_length=80)
-    security_question2 = models.CharField(null=True, max_length=80)
-    security_answer1 = models.CharField(max_length=80, null=True)
-    security_answer2 = models.CharField(max_length=80, null=True)
+    security_question1 = models.TextField(null=True, max_length=80)
+    security_question2 = models.TextField(null=True, max_length=80)
+    security_answer1 = models.TextField(max_length=80, null=True)
+    security_answer2 = models.TextField(max_length=80, null=True)
     wallet_id = models.CharField(max_length=50, default='ADCD')
 
     registration = models.DateField(null=False, default=django.utils.timezone.now)
 
+class UPreference(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    image = models.ImageField(upload_to='upload_image')
+    ep_key = models.TextField(max_length=100)
