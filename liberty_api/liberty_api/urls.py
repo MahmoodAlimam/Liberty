62dc72768650b6a09125160e708db33b43f13ae6
MahmoodAlimam <mahmood.alimam@gmail.com>
2022-09-14 16:30:09 +0100

    Installing a simple authentication process that includes login and registration for username and password

--- liberty_api/liberty_api/urls.py
+++ liberty_api/liberty_api/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('',include('wallet.urls')),
     path('admin/', admin.site.urls),
     path('wallet/',include('wallet.urls')),
-
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- liberty_api/wallet/views.py
+++ liberty_api/wallet/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("Hello, world. You are at the wallet index.")
+
+
 def index(request):
-    return HttpResponse("Hello, world. You are at the wallet index.")
+    return render(request, 'wallet/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        form.save()
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = {'form' : form}
+    return render(request, 'registration/register.html', context)
