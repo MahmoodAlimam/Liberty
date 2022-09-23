2c80ee2d345e8d5b0afd95faa2d4cb479fc5d5db
MahmoodAlimam <mahmood.alimam@gmail.com>
2022-09-23 23:21:06 +0100

    Adding the Login

--- liberty_api/liberty_api/urls.py
+++ liberty_api/liberty_api/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('',include('wallet.urls')),
     path('admin/', admin.site.urls),
-    path('wallet/',include('wallet.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
--- liberty_api/wallet/views.py
+++ liberty_api/wallet/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse
 from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
     return render(request, 'wallet/index.html')
@@ -28,6 +26,24 @@ def register(request):
 
         return redirect('index')
     else:
-        #form = UserCreationForm()
         form = NewUserForm()
     return render (request=request, template_name="registration/register.html", context={'form':form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('index')
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="registration/login.html", context={"login_form":form})
